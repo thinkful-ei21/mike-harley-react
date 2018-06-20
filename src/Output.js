d8c1f77b31971a3a35f6e24343757fdf617b1305
harleyjj <harleyyesm@gmail.com>
2018-06-20 13:35:00 -0700

    link function added

--- src/Output.js
+++ src/Output.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
 export default function Output(props) {
-    // const = props.cars;
+    const justText = <div id="output">
+                        <span>{props.make} {props.model}</span>
+                    </div>;
+    const textAndLink = <div id="output">
+                            <span>{props.make} {props.model}</span>
+                            <br></br>
+                            <a href={`https://www.autobytel.com/${props.make.toLowerCase()}/${props.model.toLowerCase()}/pictures/`} target="_blank">{props.make} {props.model}</a>
+                        </div>;
+    const returnVal = props.model ? textAndLink : justText;
 
     return (
-        <div id="output">
-            <span>{props.make} {props.model}</span>
-        </div>
+        
+            returnVal
+        
     )
 }
