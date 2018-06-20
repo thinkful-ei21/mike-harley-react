fee64573f1b9cb1bbb8c87fa2e07907c0e80d661
harleyjj <harleyyesm@gmail.com>
2018-06-20 13:42:54 -0700

    fixed minor bugs

--- src/Output.js
+++ src/Output.js
@@ -7,7 +7,7 @@ export default function Output(props) {
     const textAndLink = <div id="output">
                             <span>{props.make} {props.model}</span>
                             <br></br>
-                            <a href={`https://www.autobytel.com/${props.make.toLowerCase()}/${props.model.toLowerCase()}/pictures/`} target="_blank">{props.make} {props.model}</a>
+                            <a href={`https://www.autobytel.com/${props.make.toLowerCase()}/${props.model.toLowerCase()}/pictures/`} target="_blank">View {props.make} {props.model} Pics</a>
                         </div>;
     const returnVal = props.model ? textAndLink : justText;
 
